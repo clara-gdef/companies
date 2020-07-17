7d415639fb8c5b4c25712ae1e39aea6175b464ac
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-07-17 15:17:34 +0200

    poly disc dataset

--- data/datasets/DiscriminativePolyvalentDataset.py
+++ data/datasets/DiscriminativePolyvalentDataset.py
@@ -1,30 +1,49 @@
 import os
 import pickle as pkl
-import ipdb
-import torch
+from tqdm import tqdm
 from torch.utils.data import Dataset
 
 
 class DiscriminativePolyvalentDataset(Dataset):
-    """Face Landmarks dataset."""
+    def __init__(self, data_dir, ppl_file, rep_type, cie_reps_file, clus_reps_file, dpt_reps_file, agg_type,
+                 load=False):
+        if load:
+            with open(os.path.join(data_dir, "disc_poly_" + agg_type + ".pkl"), 'rb') as f_name:
+                self.tuples = pkl.load(f_name)
+                self.user_dim = self.tuples[0][0].shape[-1]
+                self.bag_dim = self.tuples[0][0].shape[-1]
 
-    def __init__(self, datadir, ppl_file, rep_type, agg_type, cie_reps_file, clus_reps_file, dpt_reps_file):
-        print("Loading data...")
-        with open(os.path.join(datadir, "lookup_ppl.pkl"), 'rb') as f:
-            ppl_lookup = pkl.load(f)
-        with open(os.path.join(datadir, ppl_file), 'rb') as f:
-            ppl_reps = pkl.load(f)
-        with open(os.path.join(datadir, cie_reps_file + "_" + agg_type + ".pkl"), "rb") as f:
-            cie_reps = pkl.load(f)
-        with open(os.path.join(datadir, clus_reps_file + "_" + agg_type + ".pkl"), "rb") as f:
-            clus_reps = pkl.load(f)
-        with open(os.path.join(datadir, dpt_reps_file + "_" + agg_type + ".pkl"), "rb") as f:
-            dpt_reps = pkl.load(f)
-        print("Data Loaded.")
-        self.tuples = []
-        for indices in ppl_reps:
-            ipdb.set_trace()
-
+        else:
+            print("Loading data...")
+            with open(os.path.join(data_dir, "lookup_ppl.pkl"), 'rb') as f_name:
+                ppl_lookup = pkl.load(f_name)
+            with open(os.path.join(data_dir, ppl_file), 'rb') as f_name:
+                ppl_reps = pkl.load(f_name)
+            with open(os.path.join(data_dir, cie_reps_file), "rb") as f_name:
+                cie_reps = pkl.load(f_name)
+            with open(os.path.join(data_dir, clus_reps_file), "rb") as f_name:
+                clus_reps = pkl.load(f_name)
+            with open(os.path.join(data_dir, dpt_reps_file), "rb") as f_name:
+                dpt_reps = pkl.load(f_name)
+            print("Data Loaded.")
+            self.tuples = []
+            for cie in tqdm(ppl_reps.keys(), desc="Building Discriminative Polyvalent Dataset..."):
+                for clus in ppl_reps[cie].keys():
+                    for person_id in ppl_reps[cie][clus]["id_ppl"]:
+                        self.tuples.append((ppl_lookup[person_id][rep_type],
+                                           cie_reps[ppl_lookup[person_id]["cie_label"]],
+                                           ppl_lookup[person_id]["cie_label"]))
+                        self.tuples.append((ppl_lookup[person_id][rep_type],
+                                           clus_reps[ppl_lookup[person_id]["clus_label"]],
+                                           ppl_lookup[person_id]["clus_label"]))
+                        self.tuples.append((ppl_lookup[person_id][rep_type],
+                                           dpt_reps[ppl_lookup[person_id]["dpt_label"]],
+                                           ppl_lookup[person_id]["dpt_label"]))
+            with open(os.path.join(data_dir, "disc_poly_" + agg_type + ".pkl"), 'wb') as f_name:
+                pkl.dump(self.tuples, f_name)
+            print("Discriminative Polyvalent Dataset built.")
+            print("Dataset Length: " + str(len(self.tuples)))
+            self.rep_dim = self.tuples[0][0].shape[-1]
 
     def __len__(self):
         return len(self.tuples)
