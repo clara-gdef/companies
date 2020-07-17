b3762a2832bc2e43ce2e347f886bd6b3c943f4df
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-07-17 18:17:43 +0200

    correct dataset

--- data/datasets/DiscriminativePolyvalentDataset.py
+++ data/datasets/DiscriminativePolyvalentDataset.py
@@ -1,24 +1,36 @@
 import os
 import pickle as pkl
-import ipdb
 import itertools
 import torch
 from tqdm import tqdm
 from torch.utils.data import Dataset
+import ipdb
 
 
 class DiscriminativePolyvalentDataset(Dataset):
-    def __init__(self, data_dir, ppl_file, rep_type, cie_reps_file, clus_reps_file, dpt_reps_file, agg_type,
+    def __init__(self, data_dir,
+                 ppl_file, rep_type,
+                 cie_reps_file, clus_reps_file,
+                 dpt_reps_file, agg_type,
+                 split,
                  load=False):
         if load:
-            with open(os.path.join(data_dir, "disc_poly_" + agg_type + "_" + rep_type + ".pkl"), 'rb') as f_name:
-                self = torch.load(f_name)
+            print("Loading previously saved dataset...")
+            file_name = "disc_poly_" + agg_type + "_" + rep_type + "_" + split + ".pkl"
+            with open(os.path.join(data_dir, file_name), 'rb') as f_name:
+                dic = torch.load(f_name)
+            self.rep_type = dic["rep_type"]
+            self.tuples = dic["tuples"]
+            self.rep_dim = dic["rep_dim"]
+            self.bag_rep = dic["bag_rep"]
+            print("Discriminative Polyvalent Dataset loaded.")
+            print("Dataset Length: " + str(len(self.tuples)))
 
         else:
             print("Loading data...")
             with open(os.path.join(data_dir, "lookup_ppl.pkl"), 'rb') as f_name:
                 ppl_lookup = pkl.load(f_name)
-            with open(os.path.join(data_dir, ppl_file), 'rb') as f_name:
+            with open(os.path.join(data_dir, ppl_file + '_' + split + ".pkl"), 'rb') as f_name:
                 ppl_reps = pkl.load(f_name)
             with open(os.path.join(data_dir, cie_reps_file), "rb") as f_name:
                 cie_reps = pkl.load(f_name)
@@ -28,10 +40,10 @@ class DiscriminativePolyvalentDataset(Dataset):
                 dpt_reps = pkl.load(f_name)
             print("Data Loaded.")
             self.rep_type = rep_type
-            self.tuples = self.build_ppl_tuples(ppl_reps, ppl_lookup, rep_type)
+            self.tuples = build_ppl_tuples(ppl_reps, ppl_lookup, rep_type)
             self.rep_dim = self.tuples[0]["rep"].shape[-1]
             self.bag_rep = self.build_bag_reps(cie_reps, clus_reps, dpt_reps)
-            self.save_dataset(data_dir, agg_type, rep_type)
+            self.save_dataset(data_dir, agg_type, rep_type, split)
 
             print("Discriminative Polyvalent Dataset built.")
             print("Dataset Length: " + str(len(self.tuples)))
@@ -42,27 +54,33 @@ class DiscriminativePolyvalentDataset(Dataset):
     def __getitem__(self, idx):
         return self.tuples[idx]
 
-    def save_dataset(self, data_dir, agg_type, rep_type):
-        with open(os.path.join(data_dir, "disc_poly_" + agg_type + "_" + rep_type + ".pkl"), 'wb') as f_name:
-            torch.save(self, f_name)
-
-    def build_ppl_tuples(self, ppl_reps, ppl_lookup, rep_type):
-        tuples = []
-        for cie in tqdm(ppl_reps.keys(), desc="Building Discriminative Polyvalent Dataset..."):
-            for clus in ppl_reps[cie].keys():
-                for person_id in ppl_reps[cie][clus]["id_ppl"]:
-                    tuples.append(
-                        {"id": person_id,
-                         "rep": ppl_lookup[person_id][rep_type],
-                         "cie": ppl_lookup[person_id]["cie_label"],
-                         "clus": ppl_lookup[person_id]["clus_label"],
-                         "label": ppl_lookup[person_id]["dpt_label"]
-                         }
-                    )
-        return tuples
+    def save_dataset(self, data_dir, agg_type, rep_type, split):
+        dictionary = {"rep_type": self.rep_type,
+                      "tuples": self.tuples,
+                      "rep_dim": self.rep_dim,
+                      "bag_rep": self.bag_rep}
+        file_name = "disc_poly_" + agg_type + "_" + rep_type + "_" + split + ".pkl"
+        with open(os.path.join(data_dir, file_name), 'wb') as f_name:
+            torch.save(dictionary, f_name)
 
     def build_bag_reps(self, cie_reps, clus_reps, dpt_reps):
         tmp = torch.FloatTensor(1, self.rep_dim)
         for bag in itertools.chain(cie_reps.values(), clus_reps.values(), dpt_reps.values()):
             tmp = torch.cat((tmp, bag[self.rep_type]), dim=0)
         return tmp[1:]
+
+
+def build_ppl_tuples(ppl_reps, ppl_lookup, rep_type):
+    tuples = []
+    for cie in tqdm(ppl_reps.keys(), desc="Building Discriminative Polyvalent Dataset..."):
+        for clus in ppl_reps[cie].keys():
+            for person_id in ppl_reps[cie][clus]["id_ppl"]:
+                tuples.append(
+                    {"id": person_id,
+                     "rep": ppl_lookup[person_id][rep_type],
+                     "cie": ppl_lookup[person_id]["cie_label"],
+                     "clus": ppl_lookup[person_id]["clus_label"],
+                     "label": ppl_lookup[person_id]["dpt_label"]
+                     }
+                )
+    return tuples
