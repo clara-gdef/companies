df2138aa57c4da66102aeb743971f0a426b35d9d
Clara Gainon de Forsan de Gabriac <clara.gainon-de-forsan-de-gabriac@lip6.fr>
2020-08-17 16:31:32 +0200

    fix a indexation mashup for the classes

--- data/build_lookup_dict.py
+++ data/build_lookup_dict.py
@@ -1,11 +1,47 @@
 import os
 import pickle as pkl
 import yaml
+import ipdb
 from tqdm import tqdm
 
 
 def main():
-    dat_dir = CFG["datadir"]
+    with ipdb.launch_ipdb_on_exception():
+
+        dat_dir = CFG["datadir"]
+        lookup_ppl, all_companies, all_clusters, all_dpt = build_ppl_bag_sets(dat_dir)
+        cie_dict, clus_dict, dpt_dict = build_bag_dict(dat_dir, all_companies, all_clusters, all_dpt)
+
+        rev_cie_dict = {v: k for k, v in cie_dict.items()}
+        rev_clus_dict = {v: k for k, v in clus_dict.items()}
+        rev_dpt_dict = {v: k for k, v in dpt_dict.items()}
+
+        num_cie = len(cie_dict)
+        num_clus = len(clus_dict)
+        num_dpt = len(dpt_dict)
+
+        print("Num ppl: " + str(len(lookup_ppl)))
+        for person in tqdm(lookup_ppl, desc="Adding labels to people..."):
+            cie_lab = rev_cie_dict[lookup_ppl[person]["cie_name"]]
+            assert 0 <= cie_lab <= num_cie - 1
+            lookup_ppl[person]["cie_label"] = cie_lab
+
+            clus_lab = rev_clus_dict[lookup_ppl[person]["clus_name"]]
+            assert num_cie <= clus_lab <= num_cie + num_clus - 1
+            lookup_ppl[person]["clus_label"] = clus_lab
+
+            lookup_ppl[person]["dpt_name"] = (lookup_ppl[person]["cie_name"],
+                                              lookup_ppl[person]["clus_name"])
+
+            dpt_lab = rev_dpt_dict[lookup_ppl[person]["dpt_name"]]
+            assert num_cie + num_clus <= dpt_lab <= num_cie + num_clus + num_dpt - 1
+            lookup_ppl[person]["dpt_label"] = dpt_lab
+
+        with open(os.path.join(dat_dir, 'lookup_ppl.pkl'), 'wb') as f_name:
+            pkl.dump(lookup_ppl, f_name)
+
+
+def build_ppl_bag_sets(dat_dir):
     lookup_ppl = dict()
     all_companies = set()
     all_dpt = set()
@@ -28,15 +64,19 @@ def main():
                                                  "sk": ppl_sk[cie][i]["bin_skills"][pos],
                                                  "cie_name": cie,
                                                  "clus_name": i}
+    return lookup_ppl, all_companies, all_clusters, all_dpt
+
+
+def build_bag_dict(dat_dir, all_companies, all_clusters, all_dpt):
     cie_dict = dict()
     for pos, name in enumerate(sorted(list(all_companies))):
         cie_dict[pos] = name
-    offset = pos
+    offset = pos + 1
     clus_dict = dict()
     for pos, name in enumerate(sorted(list(all_clusters))):
         rank = offset + pos
         clus_dict[rank] = name
-    offset = rank
+    offset = rank + 1
     dpt_dict = dict()
     for pos, name in enumerate(sorted(list(all_dpt))):
         dpt_dict[offset + pos] = name
@@ -53,20 +93,7 @@ def main():
     with open(os.path.join(dat_dir, 'lookup_dpt.pkl'), 'wb') as f_name:
         pkl.dump(dpt_dict, f_name)
 
-    rev_cie_dict = {v: k for k, v in cie_dict.items()}
-    rev_clus_dict = {v: k for k, v in clus_dict.items()}
-    rev_dpt_dict = {v: k for k, v in dpt_dict.items()}
-
-    print("Num ppl: " + str(len(lookup_ppl)))
-    for person in tqdm(lookup_ppl, desc="Adding labels to people..."):
-        lookup_ppl[person]["cie_label"] = rev_cie_dict[lookup_ppl[person]["cie_name"]]
-        lookup_ppl[person]["clus_label"] = rev_clus_dict[lookup_ppl[person]["clus_name"]]
-        lookup_ppl[person]["dpt_name"] = (lookup_ppl[person]["cie_name"],
-                                          lookup_ppl[person]["clus_name"])
-        lookup_ppl[person]["dpt_label"] = rev_dpt_dict[lookup_ppl[person]["dpt_name"]]
-
-    with open(os.path.join(dat_dir, 'lookup_ppl.pkl'), 'wb') as f_name:
-        pkl.dump(lookup_ppl, f_name)
+    return cie_dict, clus_dict, dpt_dict
 
 
 if __name__ == "__main__":
