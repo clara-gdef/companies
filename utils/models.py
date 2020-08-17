66b55d8acd3666640915d69e85639ad118a8d233
Clara Gainon de Forsan de Gabriac <clara.gainon-de-forsan-de-gabriac@lip6.fr>
2020-08-17 16:56:01 +0200

    add a method to turn clas number to one hot vector for test

--- utils/models.py
+++ utils/models.py
@@ -1,6 +1,26 @@
 import ipdb
+import torch
 
 
 def collate_for_disc_poly_model(batch):
-        ipdb.set_trace()
-        return None
+    ids = [i[0]["id"] for i in batch]
+    reps = [i[0]["rep"] for i in batch]
+    cies = [i[0]["cie"] for i in batch]
+    clus = [i[0]["clus"] for i in batch]
+    dpt = [i[0]["dpt"] for i in batch]
+    return ids, torch.stack(reps), cies, clus, dpt, batch[0][-1]
+
+
+def labels_to_one_hot(b_size, classes_num, total_classes):
+    one_hot = torch.zeros(b_size, total_classes)
+    for labels in classes_num:
+        for batch_num, class_num in enumerate(labels):
+            one_hot[batch_num][class_num] = 1.
+    return one_hot
+
+
+def class_to_one_hot(num_ppl, classes_num, total_classes):
+    one_hot = torch.zeros(num_ppl, total_classes)
+    for person_num, class_num in enumerate(classes_num):
+        one_hot[person_num][class_num] = 1.
+    return one_hot.cuda()
