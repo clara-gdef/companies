eca24ef42bb2a50f5069215e81fa91b6d658f39f
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-08-20 09:29:15 +0200

    make an indices only generative dataset for reproducibility and space saving

--- data/datasets/GenerativePolyvalentDataset.py
+++ data/datasets/GenerativePolyvalentDataset.py
@@ -11,10 +11,9 @@ class GenerativePolyvalentDataset(Dataset):
     def __init__(self, data_dir, ppl_file, rep_type, cie_reps_file, clus_reps_file, dpt_reps_file, agg_type, load,
                  split="TEST"):
         if load:
-            with open(os.path.join(data_dir, "gen_poly_" + agg_type + "_" + rep_type + ".pkl"), 'rb') as f_name:
+            with open(os.path.join(data_dir, "gen_poly_indices.pkl"), 'rb') as f_name:
                 dico = torch.load(f_name)
             self.tuples = dico["tuples"]
-            self.rep_dim = dico["rep_dim"]
             self.num_cie = dico["num_cie"]
             self.num_clus = dico["num_clus"]
             self.num_dpt = dico["num_dpt"]
@@ -39,38 +38,57 @@ class GenerativePolyvalentDataset(Dataset):
                     for clus in ppl_reps[cie].keys():
                         if len(ppl_reps[cie][clus].keys()) > 0:
                             try:
+                                # if person_id in ppl_reps[cie][clus]["id_ppl"]:
+                                #         self.tuples.append((ppl_lookup[person_id][rep_type],
+                                #                             cie_reps[ppl_lookup[person_id]["cie_label"]][rep_type],
+                                #                             1.))
+                                #         self.tuples.append((ppl_lookup[person_id][rep_type],
+                                #                             clus_reps[ppl_lookup[person_id]["clus_label"]][rep_type],
+                                #                             1.))
+                                #         self.tuples.append((ppl_lookup[person_id][rep_type],
+                                #                             dpt_reps[ppl_lookup[person_id]["dpt_label"]][rep_type],
+                                #                             1.))
+                                # else:
+                                #     if random.random() > 1e-4:
+                                #         self.tuples.append((ppl_lookup[person_id][rep_type],
+                                #                             cie_reps[ppl_lookup[person_id]["cie_label"]][rep_type],
+                                #                             -1.))
+                                #         self.tuples.append((ppl_lookup[person_id][rep_type],
+                                #                             clus_reps[ppl_lookup[person_id]["clus_label"]][rep_type],
+                                #                             -1.))
+                                #         self.tuples.append((ppl_lookup[person_id][rep_type],
+                                #                             dpt_reps[ppl_lookup[person_id]["dpt_label"]][rep_type],
+                                #                             -1.))
                                 if person_id in ppl_reps[cie][clus]["id_ppl"]:
-                                        self.tuples.append((ppl_lookup[person_id][rep_type],
-                                                            cie_reps[ppl_lookup[person_id]["cie_label"]][rep_type],
+                                        self.tuples.append((person_id,
+                                                            ppl_lookup[person_id]["cie_label"],
                                                             1.))
-                                        self.tuples.append((ppl_lookup[person_id][rep_type],
-                                                            clus_reps[ppl_lookup[person_id]["clus_label"]][rep_type],
+                                        self.tuples.append((person_id,
+                                                            ppl_lookup[person_id]["clus_label"],
                                                             1.))
-                                        self.tuples.append((ppl_lookup[person_id][rep_type],
-                                                            dpt_reps[ppl_lookup[person_id]["dpt_label"]][rep_type],
+                                        self.tuples.append((person_id,
+                                                            ppl_lookup[person_id]["dpt_label"],
                                                             1.))
                                 else:
-                                    if random.random() > 1e-3:
-                                        self.tuples.append((ppl_lookup[person_id][rep_type],
-                                                            cie_reps[ppl_lookup[person_id]["cie_label"]][rep_type],
+                                    if random.random() > 1e-4:
+                                        self.tuples.append((person_id,
+                                                            ppl_lookup[person_id]["cie_label"],
                                                             -1.))
-                                        self.tuples.append((ppl_lookup[person_id][rep_type],
-                                                            clus_reps[ppl_lookup[person_id]["clus_label"]][rep_type],
+                                        self.tuples.append((person_id,
+                                                            ppl_lookup[person_id]["clus_label"],
                                                             -1.))
-                                        self.tuples.append((ppl_lookup[person_id][rep_type],
-                                                            dpt_reps[ppl_lookup[person_id]["dpt_label"]][rep_type],
+                                        self.tuples.append((person_id,
+                                                            ppl_lookup[person_id]["dpt_label"],
                                                             -1.))
                             except:
                                 continue
 
             print("Generative Polyvalent Dataset built.")
             print("Dataset Length: " + str(len(self.tuples)))
-            self.rep_type = rep_type
             self.num_cie = len(cie_reps)
             self.num_clus = len(clus_reps)
             self.num_dpt = len(dpt_reps)
-            self.rep_dim = self.tuples[0][0].shape[-1]
-            self.save_dataset(data_dir, agg_type, rep_type)
+            self.save_dataset(data_dir)
 
     def __len__(self):
         return len(self.tuples)
@@ -78,11 +96,9 @@ class GenerativePolyvalentDataset(Dataset):
     def __getitem__(self, idx):
         return self.tuples[idx]
 
-    def save_dataset(self, data_dir, agg_type, rep_type):
-        with open(os.path.join(data_dir, "gen_poly_" + agg_type + "_" + rep_type + ".pkl"), 'wb') as f_name:
+    def save_dataset(self, data_dir):
+        with open(os.path.join(data_dir, "gen_poly_indices.pkl"), 'wb') as f_name:
             dico = {"tuples": self.tuples,
-                    "rep_dim": self.rep_dim,
-                    "rep_type": self.rep_type,
                     'num_cie': self.num_cie,
                     'num_clus': self.num_clus,
                     "num_dpt": self.num_dpt}
