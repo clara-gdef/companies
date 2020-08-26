3a58097455192ed9c0cb4f6e59f0a784d2546bd3
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-08-26 09:30:06 +0200

    wip

--- models/train/disc_poly.py
+++ models/train/disc_poly.py
@@ -14,9 +14,7 @@ from utils.models import collate_for_disc_poly_model
 def main(hparams):
 
     with ipdb.launch_ipdb_on_exception():
-        datasets = load_datasets(hparams, ["TRAIN", "VALID"], hparams.load_dataset)
-
-#        train(hparams)
+        train(hparams)
 
 
 def train(hparams):
@@ -121,8 +119,8 @@ if __name__ == "__main__":
     parser.add_argument("--rep_type", type=str, default='sk')
     parser.add_argument("--gpus", type=int, default=1)
     parser.add_argument("--b_size", type=int, default=64)
-    parser.add_argument("--input_type", type=str, default="userOnly")
-    parser.add_argument("--load_dataset", type=bool, default=False)
+    parser.add_argument("--input_type", type=str, default="matMul")
+    parser.add_argument("--load_dataset", type=bool, default=True)
     parser.add_argument("--data_agg_type", type=str, default="avg")
     parser.add_argument("--lr", type=float, default=1e-5)
     parser.add_argument("--epochs", type=int, default=50)
