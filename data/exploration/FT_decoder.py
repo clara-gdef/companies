92fcdb8da2b3d73a5f24cbd7d8ad88831cd57766
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-09-03 11:51:45 +0200

    lay ground for ft dec

--- data/exploration/FT_decoder.py
+++ data/exploration/FT_decoder.py
@@ -7,7 +7,7 @@ import ipdb
 
 def main():
     with ipdb.launch_ipdb_on_excpetion():
-    print("Loading data...")
+        print("Loading data...")
         with open(os.path.join(CFG["gpudatadir"], "lookup_ppl.pkl"), 'rb') as f_name:
             ppl_lookup = pkl.load(f_name)
         ft_model = fastText.load_model(os.path.join(CFG["modeldir"], "esann2020/ft_from_scratch.bin"))
