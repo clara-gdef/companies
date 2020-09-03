a7e7fc656215198fc67fc22f4e6f7fd85735df40
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-09-03 09:41:46 +0200

    update model file naming in eval

--- models/eval/disc_poly.py
+++ models/eval/disc_poly.py
@@ -44,7 +44,8 @@ def test(hparams):
 
     dataset = load_datasets(hparams, ["TEST"], hparams.load_dataset)
     test_loader = DataLoader(dataset[0], batch_size=1, collate_fn=collate_for_disc_poly_model, num_workers=32)
-    model_path = os.path.join(CFG['modeldir'], "disc_poly/" + hparams.rep_type + "/" + hparams.data_agg_type + "/" + hparams.input_type)
+    model_path = os.path.join(CFG['modeldir'], "disc_poly/" + hparams.rep_type + "/" + hparams.data_agg_type + "/" + hparams.input_type + "/" +
+                              str(hparams.b_size) + "/" + str(hparams.lr))
     model_files = glob.glob(os.path.join(model_path, "*"))
     latest_file = max(model_files, key=os.path.getctime)
     print("Evaluating model: " + str(latest_file))
