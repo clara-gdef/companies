19a6e224081a7844e2f47666b48c0684063529fb
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-09-15 16:21:42 +0200

    try multi gpu learning

--- models/grid_search_disc_spe.py
+++ models/grid_search_disc_spe.py
@@ -57,6 +57,6 @@ if __name__ == "__main__":
     parser.add_argument("--data_agg_type", type=str, default="avg")
     parser.add_argument("--middle_size", type=int, default=100)
     parser.add_argument("--epochs", type=int, default=100)
-    parser.add_argument("--bag_types", nargs='+', default=["cie", "clus"])
+    parser.add_argument("--bag_types", nargs='+', default=["cie", "clus", "dpt"])
     hparams = parser.parse_args()
     grid_search(hparams)
