47b734a9ff378245fec49318be884a4eea2f4b17
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-09-15 16:13:38 +0200

    try multi gpu learning

--- models/grid_search_disc_spe.py
+++ models/grid_search_disc_spe.py
@@ -15,14 +15,17 @@ def grid_search(hparams):
             for lr in [1e-4, 1e-6, 1e-8]:
                 test_results[lr] = {}
                 for b_size in [16, 64, 512]:
-                    print("Grid Search for " + bag_type.upper() + " (lr=" + str(lr) + ", b_size=" + str(b_size) + ")")
-                    dico['lr'] = lr
-                    dico["b_size"] = b_size
-                    dico["bag_type"] = bag_type
-                    arg = DotDict(dico)
-                    train.disc_spe.main(arg)
-                    test_results[lr][b_size] = eval.disc_spe.test(arg, CFG)
-            res_path = os.path.join(CFG["gpudatadir"], "EVAL_gs_all_disc_spe_" + bag_type + "_" + hparams.rep_type + "_" + hparams.input_type)
+                    test_results[lr][b_size] = {}
+                    for mid_size in [200, 600, 1000]:
+                        print("Grid Search for " + bag_type.upper() + " (lr=" + str(lr) + ", b_size=" + str(b_size) + ")")
+                        dico['lr'] = lr
+                        dico["b_size"] = b_size
+                        dico["bag_type"] = bag_type
+                        arg = DotDict(dico)
+                        train.disc_spe.main(arg)
+                        test_results[lr][b_size][mid_size] = eval.disc_spe.test(arg, CFG)
+            res_path = os.path.join(CFG["gpudatadir"], "EVAL_gs_all_disc_spe_" + bag_type + "_" + hparams.rep_type +
+                                    "_" + hparams.input_type)
             with open(res_path, "wb") as f:
                 pkl.dump(test_results, f)
 
@@ -36,8 +39,7 @@ def init_args(hparams):
             'data_agg_type': 'avg',
             'epochs': 50,
             "wd": 0.,
-            "load_from_checkpoint": False,
-            "middle_size": hparams.middle_size}
+            "load_from_checkpoint": False}
     return dico
 
 
