d45e816f2a3efc41bb3fba36febe4e1886dd58ae
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-09-18 11:56:28 +0200

    wip

--- data/datasets/DiscriminativePolyvalentDataset.py
+++ data/datasets/DiscriminativePolyvalentDataset.py
@@ -21,9 +21,7 @@ class DiscriminativePolyvalentDataset(Dataset):
             with open(os.path.join(data_dir, file_name), 'rb') as f_name:
                 dic = torch.load(f_name)
             self.rep_type = dic["rep_type"]
-            r = torch.randperm(14000)
-            ipdb.set_trace()
-            self.tuples = dic["tuples"][:r]
+            self.tuples = np.random.shuffle(dic["tuples"][:14000])
             self.rep_dim = dic["rep_dim"]
             self.bag_rep = dic["bag_rep"]
             self.num_cie = dic["num_cie"]
