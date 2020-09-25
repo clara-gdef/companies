73123c3da677c8f48cf5d7383ed8446c2e570153
Clara Gainon de Forsan de Gabriac <clara.gainon-de-forsan-de-gabriac@lip6.fr>
2020-09-25 16:24:23 +0200

    add job dtaaset

--- data/datasets/JobsDataset.py
+++ data/datasets/JobsDataset.py
@@ -8,16 +8,23 @@ from torch.utils.data import Dataset
 
 
 class JobsDataset(Dataset):
-    def __init__(self, data_dir, rep_type, bag_type):
+    def __init__(self, data_dir, rep_type, bag_type, bag_rep):
+        self.jobs = []
+        self.jobs_emb = []
+        self.indices = []
+        self.preds = []
+        self.labels = []
+        self.bag_rep = bag_rep
         print("Loading previously saved dataset...")
-        file_name = "jobs_and_emb_wc" + bag_type + "_" + rep_type +"_TEST.pkl"
+        file_name = "jobs_and_emb_wc_" + bag_type + "_" + rep_type +"_TEST.pkl"
         with open(os.path.join(data_dir, file_name), 'rb') as f_name:
             dic = torch.load(f_name)
-        self.jobs = dic["jobs"]
-        self.jobs_emb = dic["job_emb"]
-        self.indices = dic["indices"]
-        self.preds = dic["pred"]
-        self.labels = dic["label"]
+        for i in dic.keys():
+            self.jobs.append(dic[i]["jobs"])
+            self.jobs_emb.append(dic[i]["job_emb"])
+            self.indices.append(i)
+            self.preds.append(dic[i]["pred"])
+            self.labels.append(dic[i]["label"])
 
         # self.tuples = tmp
 
@@ -28,4 +35,4 @@ class JobsDataset(Dataset):
         return len(self.indices)
 
     def __getitem__(self, idx):
-        return self.indices[idx], self.jobs[idx], self.jobs_emb[idx], self.preds[idx], self.labels[idx]
+        return self.indices[idx], self.jobs[idx], self.jobs_emb[idx], self.preds[idx], self.labels[idx], self.bag_rep
