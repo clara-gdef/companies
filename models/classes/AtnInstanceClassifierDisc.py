501ae72d184ef10989befbf99da450cf80ab3bae
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-10-02 17:25:11 +0200

    wip

--- models/classes/AtnInstanceClassifierDisc.py
+++ models/classes/AtnInstanceClassifierDisc.py
@@ -15,7 +15,7 @@ class AtnInstanceClassifierDisc(pl.LightningModule):
         self.dim_size = dim_size
         self.input_type = hparams.input_type
         self.hparams = hparams
-        self.type = desc.split("_")[1]
+        self.type = desc.split("_")[2]
         if self.type == 'spe':
             self.bag_type = desc.split("_")[2]
 
