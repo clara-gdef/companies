3a67bafa65c88abaecabda11c382e61c04a41089
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-10-12 15:14:03 +0200

    build dataset with delta

--- data/pre_proc/subsample_according_to_delta_t.py
+++ data/pre_proc/subsample_according_to_delta_t.py
@@ -1,19 +1,12 @@
 import os
-import argparse
 import pickle as pkl
 import ipdb
 import yaml
 from tqdm import tqdm
-import re
-from collections import Counter
-import pandas as pd
-import unidecode
-from nltk.tokenize import word_tokenize
-import json
 from datetime import datetime
 
 
-def main(args):
+def main():
     global CFG
     with open("config.yaml", "r") as ymlfile:
         CFG = yaml.load(ymlfile, Loader=yaml.SafeLoader)
@@ -22,7 +15,7 @@ def main(args):
     for split in ["_TRAIN", "_VALID", "_TEST"]:
         with open(ppl_file + split + ".pkl", 'rb') as fp:
             data = pkl.load(fp)
-        dataset = build_dataset(data)
+        dataset = build_dataset(data, split)
         with open(tgt_file + split + ".pkl", 'wb') as f:
             pkl.dump(dataset)
 
@@ -62,13 +55,4 @@ def add_deltas_to_jobs(person):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--base_file", type=str, default="bp_3jobs_desc_edu_skills_industry_date_company_FR.json")
-    parser.add_argument("--index_file", type=str, default='pkl/index_40k.pkl')
-    parser.add_argument("--temporal", type=bool, default=False)
-    parser.add_argument("--MAX_SEQ_LENGTH", type=int, default=64)
-    parser.add_argument("--DATA_DIR_SRC", type=str, default="/local/gainondefor/work/lip6/data/seq2seq")
-    parser.add_argument("--DATA_DIR_TGT", type=str, default="/local/gainondefor/work/lip6/data/companies")
-    parser.add_argument("--MIN_JOB_COUNT", type=int, default=3)
-    args = parser.parse_args()
-    main(args)
+    main()
