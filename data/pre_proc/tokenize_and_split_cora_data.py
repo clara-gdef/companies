e9fa509f83d7fbcc255f750e565717dd1f53d712
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-10-16 18:02:03 +0200

    handle cora data

--- data/pre_proc/tokenize_and_split_cora_data.py
+++ data/pre_proc/tokenize_and_split_cora_data.py
@@ -10,11 +10,11 @@ from tqdm import tqdm
 from datetime import datetime
 
 
-def main(args):
+def main():
     global CFG
     with open("config.yaml", "r") as ymlfile:
         CFG = yaml.load(ymlfile, Loader=yaml.SafeLoader)
-
+    classes = set()
     with ipdb.launch_ipdb_on_exception():
         with open(os.path.join(CFG["datadir"], "classifed_papers.pkl"), 'rb') as f:
             papers = pkl.load(f)
@@ -25,10 +25,12 @@ def main(args):
                 if handle in papers[k].keys():
                     papers_transformed[k][handle] = word_tokenize(papers[k][handle])
             for handle in ["filename", "class"]:
+                if handle == "class":
+                    classes.add(papers[k][handle])
                 papers_transformed[k][handle] = papers[k][handle]
-        randomized_indices = random.shuffle([i for i in papers_transformed.keys()])
-
-        train, valid, test = []
+        randomized_indices = [i for i in papers_transformed.keys()]
+        random.shuffle(randomized_indices)
+        train, valid, test = [], [], []
         for k in randomized_indices:
             tmp = random.random()
             if tmp < .2:
@@ -38,6 +40,10 @@ def main(args):
                     valid.append((k, papers_transformed[k]))
             else:
                 train.append((k, papers_transformed[k]))
+
+        class_dict = {k: v for k, v in enumerate(sorted(classes))}
+        with open(os.path.join(CFG["datadir"], "cora_classes_dict.pkl"), 'wb') as f:
+            pkl.dump(class_dict)
 
         print("Train ratio: " + str(100 * len(train) / len(papers_transformed)) + "%")
         print("Valid ratio: " + str(100 * len(valid) / len(papers_transformed)) + "%")
@@ -54,6 +60,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    main(args)
+    main()
