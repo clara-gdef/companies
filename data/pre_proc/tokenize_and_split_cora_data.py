0f656af6c776154d4dee256e4181d33b6e0cf0b6
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-10-16 17:07:54 +0200

    handle cora data

---
+++ data/pre_proc/tokenize_and_split_cora_data.py
@@ -0,0 +1,18 @@
+import os
+import pickle as pkl
+import ipdb
+import yaml
+from nltk.tokenize import word_tokenize
+from tqdm import tqdm
+from datetime import datetime
+
+
+def main(args):
+    global CFG
+    with open("config.yaml", "r") as ymlfile:
+        CFG = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+
+    with ipdb.launch_ipdb_on_exception():
+        with open(os.path.join(CFG["datadir"], "classifed_papers.pkl"), 'rb') as f:
+            papers = pkl.load(f)
+        ipdb.set_trace()
