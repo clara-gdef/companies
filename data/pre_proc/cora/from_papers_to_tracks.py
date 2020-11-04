c77133facca2346a227e79a4746e4f3195b4b590
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-11-04 14:15:18 +0100

    relaunch from fb

--- data/pre_proc/cora/from_papers_to_tracks.py
+++ data/pre_proc/cora/from_papers_to_tracks.py
@@ -1,26 +1,34 @@
 import os
 import argparse
 import pickle as pkl
+
+import yaml
 from tqdm import tqdm
 import ipdb
 
 def main(args):
+    
+    global CFG
+    with open("config.yaml", "r") as ymlfile:
+        CFG = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+    with open(os.path.join(CFG["gpudatadir"], "cora_classes_dict.pkl"), 'rb') as f:
+        classes = pkl.load(f)
     with ipdb.launch_ipdb_on_exception():
         sets = ["_TRAIN", "_VALID", "_TEST"]
         for item in sets:
             tracks_dict = {}
-            ppl_file = os.path.join(args.DATA_DIR, args.input_file + item + ".pkl")
+            ppl_file = os.path.join(CFG["gpudatadir"], "cora" + item + ".pkl")
             with open(ppl_file, 'rb') as fp:
                 data = pkl.load(fp)
             ipdb.set_trace()
             for person in tqdm(data, desc="Build track for " + str(item)):
-                build_track_info_from_person(person, tracks_dict)
-            tgt_file = os.path.join(args.DATA_DIR, 'track' + item + ".pkl")
+                build_track_info_from_person(person, tracks_dict, classes)
+            tgt_file = os.path.join(CFG["gpudatadir"], 'track' + item + ".pkl")
             with open(tgt_file, 'wb') as fc:
                 pkl.dump(tracks_dict, fc)
 
 
-def build_track_info_from_person(person, tracks_dict):
+def build_track_info_from_person(person, tracks_dict, classes):
     identifier, skills, industry, jobs = person
     if len(jobs) > 0:
         for job in jobs:
@@ -36,7 +44,5 @@ def build_track_info_from_person(person, tracks_dict):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--DATA_DIR", type=str, default="/local/gainondefor/work/lip6/data/tracks")
-    parser.add_argument("--input_file", type=str, default="cora")
     args = parser.parse_args()
     main(args)
