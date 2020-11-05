019179e0c4b30844269714c9db30833d72f6bd30
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-11-05 15:33:39 +0100

    try disc spe cora

--- models/eval/__init__.py
+++ models/eval/__init__.py
@@ -1,3 +1,4 @@
 from .disc_poly import *
 from .disc_spe import *
 from .atn_disc_poly import *
+from .cora import *
--- models/train/__init__.py
+++ models/train/__init__.py
@@ -1,3 +1,4 @@
 from .disc_spe import *
 from .disc_poly import *
 from .atn_disc_poly import *
+from .cora import *
