4af5a259dcec49aa6c236bb2095e7d117e3d90ce
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-11-10 17:37:13 +0100

    debug for areas reps

--- data/pre_proc/cora/build_track_rep_from_train.py
+++ data/pre_proc/cora/build_track_rep_from_train.py
@@ -3,6 +3,7 @@ import argparse
 import pickle as pkl
 import yaml
 import numpy as np
+import ipdb
 from tqdm import tqdm
 
 
@@ -10,34 +11,35 @@ def main(args):
     global CFG
     with open("config.yaml", "r") as ymlfile:
         CFG = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+    with ipdb.launch_ipdb_on_exception():
+        high_level = (args.high_level_classes == "True")
+        if high_level:
+            with open(os.path.join(CFG["gpudatadir"], "cora_high_level_classes_dict.pkl"), 'rb') as f:
+                classes = pkl.load(f)
+            with open(os.path.join(CFG["gpudatadir"], "cora_track_to_hl_classes_map.pkl"), 'rb') as f:
+                high_level_mapper = pkl.load(f)
+        else:
+            with open(os.path.join(CFG["gpudatadir"], "cora_classes_dict.pkl"), 'rb') as f:
+                classes = pkl.load(f)
+            high_level_mapper = None
 
-    high_level = (args.high_level_classes == "True")
-    if high_level:
-        with open(os.path.join(CFG["gpudatadir"], "cora_high_level_classes_dict.pkl"), 'rb') as f:
-            classes = pkl.load(f)
-        with open(os.path.join(CFG["gpudatadir"], "cora_track_to_hl_classes_map.pkl"), 'rb') as f:
-            high_level_mapper = pkl.load(f)
-    else:
-        with open(os.path.join(CFG["gpudatadir"], "cora_classes_dict.pkl"), 'rb') as f:
-            classes = pkl.load(f)
-        high_level_mapper = None
-    tgt_file = os.path.join(CFG["gpudatadir"], "cora_embedded_" + args.ft_type + "_TRAIN.pkl")
-    with open(tgt_file, 'rb') as f:
-        train_dataset = pkl.load(f)
+        tgt_file = os.path.join(CFG["gpudatadir"], "cora_embedded_" + args.ft_type + "_TRAIN.pkl")
+        with open(tgt_file, 'rb') as f:
+            train_dataset = pkl.load(f)
 
-    classes_rep = []
-    for class_num in tqdm(classes.keys()):
-        papers = select_relevant_papers(train_dataset, class_num, high_level_mapper)
-        classes_rep.append(np.mean(np.stack(papers), axis=0))
+        classes_rep = []
+        for class_num in tqdm(classes.keys()):
+            papers = select_relevant_papers(train_dataset, class_num, high_level_mapper)
+            classes_rep.append(np.mean(np.stack(papers), axis=0))
 
-    assert len(classes_rep) == len(classes)
+        assert len(classes_rep) == len(classes)
 
-    if high_level:
-        tgt_file = os.path.join(CFG["gpudatadir"], "areas_reps_" + args.ft_type + ".pkl")
-    else:
-        tgt_file = os.path.join(CFG["gpudatadir"], "tracks_reps_" + args.ft_type + ".pkl")
-    with open(tgt_file, 'wb') as f:
-        pkl.dump(classes_rep, f)
+        if high_level:
+            tgt_file = os.path.join(CFG["gpudatadir"], "areas_reps_" + args.ft_type + ".pkl")
+        else:
+            tgt_file = os.path.join(CFG["gpudatadir"], "tracks_reps_" + args.ft_type + ".pkl")
+        with open(tgt_file, 'wb') as f:
+            pkl.dump(classes_rep, f)
 
 
 def select_relevant_papers(train_dataset, class_num, high_level_mapper):
@@ -54,7 +56,7 @@ def select_relevant_papers(train_dataset, class_num, high_level_mapper):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ft_type", type=str, default="ft_en.bin")
+    parser.add_argument("--ft_type", type=str, default="fs")
     parser.add_argument("--high_level_classes", type=str, default="True")
     args = parser.parse_args()
     main(args)
