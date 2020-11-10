5a5d7c1ba7261cbb5873618c02fdde660a13e1b7
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-11-10 14:03:49 +0100

    first draft of cora bow svm

--- models/baselines/cora_bow_svm.py
+++ models/baselines/cora_bow_svm.py
@@ -5,6 +5,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import LinearSVC
 import ipdb
 from nltk.corpus import stopwords
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, confusion_matrix
 
 from tqdm import tqdm
 
@@ -13,28 +14,70 @@ def main():
     global CFG
     with open("config.yaml", "r") as ymlfile:
         CFG = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+
+    with open(os.path.join(CFG["gpudatadir"], "cora_classes_dict.pkl"), 'rb') as f:
+        class_dict = pkl.load(f)
+
     paper_file = os.path.join(CFG["gpudatadir"], CFG["rep"]["cora"]["papers"]["plain"] + "TRAIN.pkl")
     with open(paper_file, 'rb') as f:
         data_train = pkl.load(f)
 
-    cleaned_data = pre_proc_data(data_train)
+    cleaned_abstracts, labels = pre_proc_data(data_train, class_dict)
+    train_features = fit_vectorizer(cleaned_abstracts)
+    model = train_svm(train_features, labels)
 
     paper_file = os.path.join(CFG["gpudatadir"], CFG["rep"]["cora"]["papers"]["plain"] + "TEST.pkl")
     with open(paper_file, 'rb') as f:
         data_test = pkl.load(f)
 
-
-def pre_proc_data(data):
-    # tokenize
-    # remove stop words
-    sw = set(stopwords.words("english"))
-    # fit vectorizer
-
+    cleaned_abstracts_test, labels_test = pre_proc_data(data_test, class_dict)
+    test_features = fit_vectorizer(cleaned_abstracts_test)
+    predicted_labels = model.predict(test_features)
+    print(model.score(test_features, labels_test))
     ipdb.set_trace()
 
+    res = eval_model(labels_test, predicted_labels)
 
-def train_svm():
-    pass
+    print(res)
+
+
+
+def pre_proc_data(data, class_dict):
+    stop_words = set(stopwords.words("english"))
+    labels = []
+    abstracts = []
+    for article in tqdm(data, desc="Parsing articles..."):
+        if "Abstract" in article[1].keys() and "class" in article[1].keys():
+            labels.append(class_dict[article[1]["class"]])
+            cleaned_ab = [w for w in article[1]["Abstract"] if w not in stop_words]
+            abstracts.append(cleaned_ab)
+    return abstracts, labels
+
+
+def fit_vectorizer(input_data):
+    vectorizer = CountVectorizer(analyzer="word", tokenizer=None, preprocessor=None, stop_words=None, max_features=1500)
+    print("Fitting vectorizer...")
+    data_features = vectorizer.fit_transform(input_data)
+    print("Vectorizer fitted.")
+    data_features = data_features.toarray()
+    return data_features
+
+
+def train_svm(data, labels):
+    model = LinearSVC()
+    print("Fitting SVM...")
+    model.fit(data, labels)
+    print("SVM fitted!")
+    return model
+
+def eval_model(labels, predicted_labels):
+    num_c = range(offset, offset + num_classes)
+    res_dict = {
+        "acc_" + handle: accuracy_score(labels, preds) * 100,
+        "precision_" + handle: precision_score(labels, preds, average='weighted',
+                                               labels=num_c, zero_division=0) * 100,
+        "recall_" + handle: recall_score(labels, preds, average='weighted', labels=num_c, zero_division=0) * 100,
+        "f1_" + handle: f1_score(labels, preds, average='weighted', labels=num_c, zero_division=0) * 100}
 
 
 if __name__ == "__main__":
