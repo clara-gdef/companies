db9cf581e93c1c4c74e352759f282584b0108e0b
Clara Gainon de Forsan <clara.gdef@gmail.com>
2020-12-15 16:40:28 +0100

    update atn dis spe

--- models/grid_search_atn_disc_spe.py
+++ models/grid_search_atn_disc_spe.py
@@ -8,38 +8,37 @@ from utils import DotDict
 
 
 def grid_search(hparams):
-    with ipdb.launch_ipdb_on_exception():
-        test_results = {}
-        dico = init_args(hparams)
-        for str_lr in hparams.lr:
-            lr = float(str_lr)
-            test_results[lr] = {}
-            for b_size in hparams.b_size:
-                test_results[lr][int(b_size)] = {}
-                if hparams.input_type == "hadamard":
-                    for mid_size in [50, 200, 600]:
-                        print("Grid Search for (lr=" + str(lr) + ", b_size=" + str(b_size) + ", middle size=" + str(mid_size) + ")")
-                        dico['lr'] = lr
-                        dico["b_size"] = b_size
-                        dico["middle_size"] = mid_size
-                        arg = DotDict(dico)
-                        if hparams.TRAIN == "True":
-                            train.atn_disc_spe.init(arg)
-                        test_results[lr][b_size][mid_size] = eval.atn_disc_spe.main(arg)
-                else:
-                    print("Grid Search for (lr=" + str(lr) + ", b_size=" + str(b_size) + ")")
+    test_results = {}
+    dico = init_args(hparams)
+    for str_lr in hparams.lr:
+        lr = float(str_lr)
+        test_results[lr] = {}
+        for b_size in hparams.b_size:
+            test_results[lr][int(b_size)] = {}
+            if hparams.input_type == "hadamard":
+                for mid_size in [50, 200, 600]:
+                    print("Grid Search for (lr=" + str(lr) + ", b_size=" + str(b_size) + ", middle size=" + str(mid_size) + ")")
                     dico['lr'] = lr
                     dico["b_size"] = b_size
-                    dico["middle_size"] = hparams.middle_size
+                    dico["middle_size"] = mid_size
                     arg = DotDict(dico)
                     if hparams.TRAIN == "True":
                         train.atn_disc_spe.init(arg)
+                    test_results[lr][b_size][mid_size] = eval.atn_disc_spe.main(arg)
+            else:
+                print("Grid Search for (lr=" + str(lr) + ", b_size=" + str(b_size) + ")")
+                dico['lr'] = lr
+                dico["b_size"] = b_size
+                dico["middle_size"] = hparams.middle_size
+                arg = DotDict(dico)
+                if hparams.TRAIN == "True":
+                    train.atn_disc_spe.init(arg)
 
-                    test_results[lr][b_size] = eval.atn_disc_spe.init(arg)
-            ## TODO REMOVE THIS - UNINDENT
-            res_path = os.path.join(CFG["gpudatadir"], "EVAL_gs_" + hparams.model_type + "_topK_disc_spe_" + hparams.rep_type + "_" + hparams.input_type)
-            with open(res_path, "wb") as f:
-                pkl.dump(test_results, f)
+                test_results[lr][b_size] = eval.atn_disc_spe.init(arg)
+        ## TODO REMOVE THIS - UNINDENT
+        res_path = os.path.join(CFG["gpudatadir"], "EVAL_gs_" + hparams.model_type + "_topK_disc_spe_" + hparams.rep_type + "_" + hparams.input_type)
+        with open(res_path, "wb") as f:
+            pkl.dump(test_results, f)
 
 
 def init_args(hparams):
@@ -76,7 +75,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_agg_type", type=str, default="avg")
     parser.add_argument("--frozen", type=str, default="False")
     parser.add_argument("--init_weights", type=str, default="False")
-    parser.add_argument("--DEBUG", type=bool, default=False)
+    parser.add_argument("--DEBUG", default="False")
     parser.add_argument("--model_type", type=str, default="atn_disc_spe_new")
     parser.add_argument("--middle_size", type=int, default=50)
     parser.add_argument("--epochs", type=int, default=10)
