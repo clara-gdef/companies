0cd8fb97ef390a4bd967f79ac5032fd4d537c4c5
Clara Gainon de Forsan <clara.gdef@gmail.com>
2021-01-05 11:52:20 +0100

    draft of word dataset building

--- data/pre_proc/build_word_dataset.py
+++ data/pre_proc/build_word_dataset.py
@@ -14,8 +14,7 @@ def main(hparams):
         print("Loading word vectors...")
         if hparams.rep_type == "ft":
             tmp = fastText.load_model(os.path.join(CFG["modeldir"], "ft_fs.bin"))
-            embedder = tmp.get_word_vector()
-        datasets = []
+            embedder = tmp.get_word_vector
         common_hparams = {
             "data_dir": CFG["gpudatadir"],
             "rep_type": hparams.rep_type,
@@ -26,29 +25,15 @@ def main(hparams):
             "subsample":0
         }
         for split in ["TRAIN", "VALID", "TEST"]:
-            datasets.append(WordDatasetSpe(**common_hparams, split=split))
+            WordDatasetSpe(**common_hparams, split=split)
         print("all datasets have been built!")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--rep_type", type=str, default='ft')
-    parser.add_argument("--gpus", type=int, default=1)
-    parser.add_argument("--b_size", type=int, default=768)
-    parser.add_argument("--middle_size", type=int, default=20)
-    parser.add_argument("--input_type", type=str, default="matMul")
+    parser.add_argument("--min_job_count", type=int, default=3)
     parser.add_argument("--load_dataset", default="True")
-    parser.add_argument("--init_weights", default="True")
-    parser.add_argument("--auto_lr_find", type=bool, default=False)
-    parser.add_argument("--load_from_checkpoint", default=False)
-    parser.add_argument("--standardized", type=str, default="True")
-    parser.add_argument("--checkpoint", type=int, default=45)
-    parser.add_argument("--data_agg_type", type=str, default="avg")
-    parser.add_argument("--DEBUG", type=bool, default=False)
     parser.add_argument("--subsample", type=int, default=0)
-    parser.add_argument("--model_type", type=str, default="atn_disc_poly_std")
-    parser.add_argument("--lr", type=float, default=1e-8)
-    parser.add_argument("--wd", type=float, default=0.7)
-    parser.add_argument("--epochs", type=int, default=50)
     hparams = parser.parse_args()
     main(hparams)
